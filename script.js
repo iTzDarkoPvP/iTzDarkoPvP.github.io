d9b071aa3368d8e8fd1582b022800c1b4de2d7b7
iTzDarkoPvP <mathiasgamingxd81@gmail.com>
2025-05-22 19:35:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,6 @@ document.getElementById("surpriseBtn").addEventListener("click", function() {
         specialMessage.id = "specialMessage";
         specialMessage.innerHTML = "❤️🫶🏻 Te amo pendejita 🫶🏻❤️";
         document.body.appendChild(specialMessage);
+        specialMessage.style.display = "block"; // Asegura que aparezca
     }, 1000);
 });
