7868d431f4022c9332db514c133c499735e767d3
iTzDarkoPvP <mathiasgamingxd81@gmail.com>
2025-05-22 19:10:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 window.addEventListener("load", function() {
-    let bgImage = new Image();
-    bgImage.src = "fondo.jpg";
-
-    bgImage.onload = function() {
-        document.body.style.backgroundImage = "url('c3fb435d224eeedae9486ea8778b1746.jpg')";
-        document.getElementById("preloader").style.display = "none";
-    };
+    document.getElementById("preloader").style.display = "none";
 });
 
 document.getElementById("surpriseBtn").addEventListener("click", function() {
     const message = document.getElementById("message");
-    message.innerText = "Eres la mejor amiga del mundo 🌟✨";
+    message.innerText = "Te amo pendejita 🫶🏻❤️";
     message.style.animation = "fadeIn 2s ease-in-out";
 });
